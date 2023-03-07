d9e3da8284e3e3ed657c036dc9cda8aebe0af5e4
Maticzpl <maticzpl06@gmail.com>
2023-03-07 18:06:28 +0100

    Fix macro added in previous commit

--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
 # PowderRS
 I'm doing tpt clones in every new lang I learn now :P
+
+## Compilation
+1. Install rust if you haven't already: https://rustup.rs/
+2. Clone the repo
+3. Open a terminal in the main directory of the cloned repo
+4. Install rust nightly: `rustup toolchain install nightly`
+5. Use rust nightly in this project: `rustup override set nightly`
+6. Build and run the project: `cargo run --release`
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use proc_macro::{TokenStream, TokenTree};
 use std::string::{String};
 use std::any::{Any, TypeId};
 use std::{env, fs};
+use std::cmp::Ordering::{Greater, Less};
+use regex::Regex;
 
 //ik this is garbage, just learning macros :P
 #[proc_macro]
@@ -16,12 +18,32 @@ pub fn get_part_types_in_dir(stream: TokenStream) -> TokenStream {
     };
 
     let path = format!("{}/{}", env::current_dir().unwrap().to_str().unwrap(), dir.unwrap());
-    let files: Vec<_> = fs::read_dir(path.clone())
-        .expect(format!("Can't open dir {}", path).as_str())
+    let mut files: Vec<_> = fs::read_dir(path.clone())
+        .expect(format!("Can't find dir {}", path).as_str())
         .map(|file| file.unwrap().path())
         .collect();
+
+    //ngl this is really awful xdd
+    files.sort_by(|a, b| {
+        if !a.is_file() || a.file_name().unwrap().to_str().unwrap() == "mod.rs" {
+            return Less;
+        }
+        if !b.is_file() || b.file_name().unwrap().to_str().unwrap() == "mod.rs" {
+            return Greater;
+        }
+
+        let reg = Regex::new(r"(.*\n)*.*(const\s*PT_.*(\n.*)*id\s*:\s*)(?P<res>\d+)(,)(.*\n?)*").unwrap();
+
+        let va : u32 = reg.replace_all(fs::read_to_string(a).unwrap().as_str(), "$res").parse().unwrap();
+        let vb : u32 = reg.replace_all(fs::read_to_string(b).unwrap().as_str(), "$res").parse().unwrap();
+
+        va.cmp(&vb)
+    });
+
+    let len = files.len() - 1; //to lazy to do proper counting
+
     let mut imports: String = String::new();
-    let mut strc: String = format!("pub const PT_TYPES : [PartType; {}] = [", files.len() - 1); //to lazy to do proper counting
+    let mut strc: String = format!("pub const PT_TYPES : [PartType; {}] = [", len);
     for path in files {
         if path.is_file() && path.file_name().unwrap() != "mod.rs" {
             let type_ident = path.file_name()
@@ -33,5 +55,8 @@ pub fn get_part_types_in_dir(stream: TokenStream) -> TokenStream {
     }
     strc.remove(strc.len()-1);
     strc += "];";
-    return format!("{}\n{}",imports,strc).parse().unwrap();
+
+    return format!("{}\n{}", imports, strc).parse().unwrap();
 }
+
+//  (const\s*PT_.*(\n.*)*id\s*:\s*)(\d+)(\,)
